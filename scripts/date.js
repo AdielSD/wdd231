ea68eaca83fb2d0dc8c04c5400105d8561310882
AdielSD <sephorazrpm@gmail.com>
2026-04-01 15:07:43 -0500

    Week 02

--- scripts/date.js
+++ scripts/date.js
@@ -3,3 +3,14 @@ year.textContent = new Date().getFullYear();
 
 document.querySelector("#lastModified").textContent =
 "Last Modified: " + document.lastModified;
+
+
+/* Hamburguer button */
+
+const hamBtn = document.querySelector("#ham-btn");
+const nav = document.querySelector("#nav-bar");
+
+hamBtn.addEventListener("click", () => {
+    nav.classList.toggle("show");
+    hamBtn.classList.toggle("show");
+}); 
